008b45321d8bc24098316d1a6475261c5a71a192
Hoàng Hưng <HungHoang31024>
2022-11-24 11:37:41 +0700

    Form1.cs

---
+++ Buoi07_TH/Bai_6.4/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Bai_6._4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnChon = new System.Windows.Forms.Button();
+            this.btnHuy = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbThanhtien = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(72, 22);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1102, 402);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // btnChon
+            // 
+            this.btnChon.Location = new System.Drawing.Point(81, 542);
+            this.btnChon.Name = "btnChon";
+            this.btnChon.Size = new System.Drawing.Size(165, 66);
+            this.btnChon.TabIndex = 1;
+            this.btnChon.Text = "Chọn";
+            this.btnChon.UseVisualStyleBackColor = true;
+            this.btnChon.Click += new System.EventHandler(this.btnChon_Click);
+            // 
+            // btnHuy
+            // 
+            this.btnHuy.Location = new System.Drawing.Point(524, 542);
+            this.btnHuy.Name = "btnHuy";
+            this.btnHuy.Size = new System.Drawing.Size(156, 66);
+            this.btnHuy.TabIndex = 2;
+            this.btnHuy.Text = "Huỷ";
+            this.btnHuy.UseVisualStyleBackColor = true;
+            this.btnHuy.Click += new System.EventHandler(this.btnHuy_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(959, 542);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(182, 66);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Thoát";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 480);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 28);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Thành Tiền:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lbThanhtien
+            // 
+            this.lbThanhtien.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.lbThanhtien.Location = new System.Drawing.Point(156, 467);
+            this.lbThanhtien.Name = "lbThanhtien";
+            this.lbThanhtien.Size = new System.Drawing.Size(167, 41);
+            this.lbThanhtien.TabIndex = 5;
+            this.lbThanhtien.Click += new System.EventHandler(this.lbThanhtien_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 28F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 630);
+            this.Controls.Add(this.lbThanhtien);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnHuy);
+            this.Controls.Add(this.btnChon);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Button btnChon;
+        private Button btnHuy;
+        private Button btnExit;
+        private Label label1;
+        private Label lbThanhtien;
+    }
+}
---
+++ Buoi07_TH/Bai_6.4/Form1.cs
@@ -0,0 +1,80 @@
+﻿namespace Bai_6._4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int TongTien = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Label[] labels = new Label[30];
+            for(int i = 0; i< 30; i++)
+            {
+                labels[i] = new Label();
+                labels[i].Width = 90;
+                labels[i].Height = 95;
+                labels[i].BackColor = Color.White;
+                labels[i].BorderStyle = BorderStyle.Fixed3D;
+                labels[i].Margin = new Padding(3, 3, 3, 10);
+                labels[i].Text = (i + 1).ToString();
+                labels[i].Click += new EventHandler(label1_Click);
+                flowLayoutPanel1.Controls.Add(labels[i]);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Label lbchon = (Label)sender;
+            if (lbchon.BackColor == Color.White)
+            {
+                lbchon.BackColor = Color.Blue;
+                TongTien += 100000;
+                lbThanhtien.Text = TongTien + "VND";
+            }
+            else if (lbchon.BackColor == Color.Blue)
+            {
+                lbchon.BackColor = Color.White;
+                lbThanhtien.Text = TongTien + "VND";
+            }
+            else if (lbchon.BackColor == Color.Yellow)
+            {
+                MessageBox.Show("Ghế này đã được bán", "Đã Bán", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnChon_Click(object sender, EventArgs e)
+        {
+            foreach(Label label in flowLayoutPanel1.Controls)
+            {
+                if(label.BackColor == Color.Blue)
+                {
+                    label.BackColor = Color.Yellow;
+                    TongTien = 0;
+                    lbThanhtien.Text = TongTien + "VND";
+                }
+            }
+        }
+
+        private void btnHuy_Click(object sender, EventArgs e)
+        {
+            foreach(Label label in flowLayoutPanel1.Controls)
+            {
+                if(label.BackColor == Color.Blue)
+                {
+                    label.BackColor = Color.White;
+                    TongTien = 0;
+                    lbThanhtien.Text = TongTien + "VND";
+                }
+            }
+        }
+
+      
+
+        private void lbThanhtien_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
